e1ca9bb02f91080d235e90e112534286f115ad49
Leon Schmitz <refardeon89@gmail.com>
2025-12-21 16:54:20 +0100

    Fix Favicon/Logo difference in Background

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,17 +1,18 @@
 <script lang="ts">
-    import favicon from '$lib/assets/HL2.svg';
+    import favicon from '$lib/assets/HL2_favicon.svg';
+    import logo from '$lib/assets/HL2.svg';
 
     let {children} = $props();
 </script>
 
 <svelte:head>
-    <link rel="icon" href={favicon}/>
+    <link rel="icon" href={favicon} style="background-color: white"/>
 </svelte:head>
 
 
 <header class="p-3 bg-amber-50">
     <a href="/">
-        <img src={favicon} class="w-12 h-12 inline mx-5" alt="HL"/>
+        <img src={logo} class="w-12 h-12 inline mx-5" alt="HL"/>
     </a>
     <h1 class="inline">Hanna und Leon</h1>
 </header>
