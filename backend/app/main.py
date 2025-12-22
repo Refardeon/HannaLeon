d208572428d96bda03babceb529b8d3dfdda8b5f
Leon Schmitz <refardeon89@gmail.com>
2025-12-22 12:48:37 +0100

    Backend: Allow Cross Origin requests from frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -15,7 +15,7 @@ app = FastAPI(title="HannaLeon", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "https://hanna-und-leon.de"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
